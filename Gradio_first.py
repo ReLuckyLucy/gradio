5373f8ed4e4c0befa1322d3c788d162077b92175
ReLuckyLucy <1257716130@qq.com>
2025-01-08 17:16:23 +0800

    first

---
+++ Gradio_first.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+def greet(你的名字, intensity):
+    return "你好, " + 你的名字 + ",你的名字真好听" +"!"* int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()#可在里面添加：share=True，使得其调用反向代理，并使得其能在公网下让他人访问你的代码
+with gr.Blocks(theme=gr.themes.Soft()) as demo:
+    ...
